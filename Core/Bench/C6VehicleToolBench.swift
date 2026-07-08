d440e1237765e6713c7c8f3f28743a5556dc4d75
王磊 <wanglei@wangleideMacBook-Pro-3.local>
2026-07-08 18:34:53 +0800

    refactor: move C6ToolCall to contracts

--- Core/Bench/C6VehicleToolBench.swift
+++ Core/Bench/C6VehicleToolBench.swift
@@ -31,16 +31,6 @@ public enum C6Bucket: String, Codable, CaseIterable, Sendable {
     case coverage
 }
 
-public struct C6ToolCall: Codable, Equatable, Sendable {
-    public var name: String
-    public var arguments: [String: String]
-
-    public init(name: String, arguments: [String: String] = [:]) {
-        self.name = name
-        self.arguments = arguments
-    }
-}
-
 public struct C6ReadbackAssertion: Codable, Equatable, Sendable {
     public var contains: [String]
 
